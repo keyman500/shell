90c7a63b5ef6bcacaa2a0f95d4edb81e25a335c3
Marc Hypolite <mar-chy@hotmail.com>
2021-02-16 07:06:56 +0000

    tring too strip input of new line character

--- main.c
+++ main.c
@@ -4,9 +4,8 @@
 #include <sys/stat.h>
 #define maxchar 50
 //this function will execute a given file on a new thread.
-void execute_file(char * filepath){
+void execute_file(char *args[]){
  int pid = 0;
- char *args[]={filepath,NULL};
     pid = fork();
     if(pid==0){
         execv(args[0],args); 
@@ -17,10 +16,20 @@ void execute_file(char * filepath){
 //returns 1 if file exists in current directory and 0 if it does not
 int file_exists(char *path){
 struct stat stats;
+printf("%smk",path);
 if (stat(path, &stats) == 0){
     return 1;
 }
 return 0;
+}
+
+char *strip(char * str){
+    int i=0;
+while(str[i]!=NULL){
+i++;
+}
+str[i-1] = NULL;
+return str;
 }
 
 int main(void) {
@@ -28,15 +37,29 @@ int main(void) {
   // printf("PATH : %s\n", getenv("PATH"));
 
     char com[maxchar]; 
+    char *list[10]={};
+    int i = 0;
     printf("[Enter command]>");
     fflush(stdout);
     fgets(com,maxchar, stdin);  
-    printf("%s",com);
-/*
+    //filling array
+	char delim[] = " ";
+	char *ptr = strtok(com, delim);
+	while(ptr != NULL)
+	{
+		printf("'%s'\n", ptr);
+        list[i] = ptr;
+		ptr = strtok(NULL, delim);
+        i++;
+	}
+    list[i] = NULL;
+
+
+
     while(strcmp(com,"exit")){
    // printf("command = %s",com);
-     if(file_exists(com)) 
-     execute_file(com);
+     if(file_exists(list[0])) 
+     execute_file(list);
      else
      printf("file not found!");
      printf("\n[Enter command]>");
@@ -44,7 +67,7 @@ int main(void) {
      scanf("%s",com); 
     }
 
-*/
+
   
     return 0;
 }
